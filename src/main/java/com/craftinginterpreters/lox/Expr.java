21ecd7f7092348385e1cb3fb61900f82f44de2a2
Igor Guerrero <igorgue@protonmail.com>
2023-03-03 19:14:53 -0600

    non compiled wip one

--- src/main/java/com/craftinginterpreters/lox/Expr.java
+++ src/main/java/com/craftinginterpreters/lox/Expr.java
@@ -5,18 +5,12 @@ import java.util.List;
 abstract class Expr {
   interface Visitor<R> {
     R visitAssignExpr(Assign expr);
-
     R visitBinaryExpr(Binary expr);
-
     R visitGroupingExpr(Grouping expr);
-
     R visitLiteralExpr(Literal expr);
-
     R visitUnaryExpr(Unary expr);
-
     R visitVariableExpr(Variable expr);
   }
-
   static class Assign extends Expr {
     Assign(Token name, Expr value) {
       this.name = name;
@@ -30,7 +24,6 @@ abstract class Expr {
     final Token name;
     final Expr value;
   }
-
   static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
@@ -46,7 +39,6 @@ abstract class Expr {
     final Token operator;
     final Expr right;
   }
-
   static class Grouping extends Expr {
     Grouping(Expr expression) {
       this.expression = expression;
@@ -58,7 +50,6 @@ abstract class Expr {
 
     final Expr expression;
   }
-
   static class Literal extends Expr {
     Literal(Object value) {
       this.value = value;
@@ -70,7 +61,6 @@ abstract class Expr {
 
     final Object value;
   }
-
   static class Unary extends Expr {
     Unary(Token operator, Expr right) {
       this.operator = operator;
@@ -84,7 +74,6 @@ abstract class Expr {
     final Token operator;
     final Expr right;
   }
-
   static class Variable extends Expr {
     Variable(Token name) {
       this.name = name;
